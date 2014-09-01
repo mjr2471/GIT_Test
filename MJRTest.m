2ced8574e994471431de6c600acadff4c5bfa77d
Mark Rascoe <markrascoe@Marks-MacBook-Air.local>
2014-08-31 19:04:31 -0700

    added test class

---
+++ MJRTest.m
@@ -0,0 +1,13 @@
+//
+//  MJRTest.m
+//  GIT Test
+//
+//  Created by Mark Rascoe on 8/31/14.
+//  Copyright (c) 2014 Rascoe Inc. All rights reserved.
+//
+
+#import "MJRTest.h"
+
+@implementation MJRTest
+
+@end
